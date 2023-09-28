1c0ec670b69489a9f8f2a4354305c6cd35385508
OTSUGUA14 <146036948+OTSUGUA14@users.noreply.github.com>
2023-09-28 14:27:18 -0300

    Trabajo de ejercicios complementarios

---
+++ ejercicios complementarios.py
@@ -0,0 +1,70 @@
+#ejercicio 1 
+numero1=14
+print("el primer numero es: " , numero1)
+#ejercicio 2
+numero2=23.06
+print("el segundo numero es: " , numero2)
+
+#ejercicio 3 
+suma= numero1 + numero2
+print("La suma es: ", suma)
+#ejercicio 4 
+resta= numero1 - numero2
+multiplicación= numero1 * numero2
+división= numero1 / numero2
+print("La resta es: ", resta)
+print("La multiplicacón es: ", multiplicación)
+print("La devisión es: ", división)
+#ejercicio 5 
+nombre="Augusto"
+print("Mi nombre es: ", nombre)
+#ejercicio 6 
+precio=1014
+#ejercicio 7 
+descuento=0.25
+#ejercicio 8 
+precio_final=precio *descuento
+print("El precio normal es: ", precio)
+print("El precio con 25 porciento de descuento es: ", precio_final)
+#ejercicio 9 
+cadena= "El salto del papu"
+#ejercicio 10 
+longitud=len(cadena)
+print("la longitud de: " ,cadena, "es de: ",longitud)
+#ejercicio 11
+precio=12.34
+print("Precio decimal: ", precio)
+precio=int(precio)
+print("Precio entero: ", precio)
+#ejercicio 12
+nombre="Augusto"
+print("la variable nombre es :", nombre)
+apeliido="Riquelme"
+print("la variable apellido es :", apeliido)
+nombre_riquelme=nombre+" "+apeliido
+print("la variable nombre_riquelme es :", nombre_riquelme)
+#ejericio 13
+edad=18
+print("Mi edad: ", edad)
+edad+=5
+print("Mi edad sumado 5: ", edad)
+edad-=10
+print("Mi edad sumado 5 y restado 10: ", edad)
+
+#ejercicio 14
+altura=1.73
+print("Mi altura: ", altura)
+altura*=4
+print("Mi altura por 4: ", altura)
+altura/=3
+print("Mi altura por 4 dividio 3: ", altura)
+#ejercicio 15
+nombre_mayus="AUGUSTO"
+print("Mi nombre mayusculas: ", nombre_mayus)
+
+nombre_minus=nombre_mayus.lower()
+print("Mi nombre en minusculas: ", nombre_minus)
+
+#ejercicioo 16
+nombre_primera=nombre_mayus.capitalize()
+print("Mi nombres es: ", nombre_primera)
